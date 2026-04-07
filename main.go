cca5b6e31c7b6b39297a84c210bc1af74a5f2a27
Scott Yoder <syoder89@gmail.com>
2026-04-07 17:46:29 -0500

    fix: add http serve shutdown timeout to prevent hanging with open connections

--- main.go
+++ main.go
@@ -248,7 +248,11 @@ func (m *monitor) serveProbes(ctx context.Context) {
 	}
 	go func() {
 		<-ctx.Done()
-		srv.Shutdown(context.Background()) //nolint:errcheck
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer shutdownCancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.Printf("WARN: probe server shutdown: %s", err)
+		}
 	}()
 	log.Printf("Probe/metrics server listening on %s", m.cfg.probeAddr)
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
